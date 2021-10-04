2a574884e803f661ebecf94000e5e572fb8bda5a
Keerbi Liu <kirby.liu12@gmail.com>
2021-10-04 12:57:50 +1100

    Added category route for get all categories, defined models for product tags and tags.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,17 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categories = await Category.findAll({
+      include: [{ model: Product }],
+    })
+    return res.status(200).json(categories)
+  } catch (err) {
+    return res.status(400).json(err)
+  }
 });
 
 router.get('/:id', (req, res) => {
